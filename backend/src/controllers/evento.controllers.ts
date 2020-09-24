485ded4edde7c881f76ad52ecc6910c7a774db9b
Dvid777 <DSegura2001@gmail.com>
2020-09-23 22:20:29 -0300

    Tareas

--- backend/src/controllers/evento.controllers.ts
+++ backend/src/controllers/evento.controllers.ts
@@ -16,7 +16,7 @@ export class EventoController
 
     public async crearEvento (req:Request, res:Response)
     {
-        let evento:IEvento = req.body;
+        let evento = req.body;
 
         const conec = await conexion();
 
