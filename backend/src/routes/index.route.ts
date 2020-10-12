da1312c69f7ba88a669e5c22e23ec508e23623a0
unknown <braianvillarroel90@gmail.com>
2020-10-12 19:01:40 -0300

    modificacion del archivo index.route

--- backend/src/routes/index.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { mensaje } from "../controllers/index.controllers";
-
-let enrutadorIndex = Router();
-
-enrutadorIndex.route('/').get(mensaje);
-
-export default enrutadorIndex;
