95c6ccd1688a129067a9cb3d8e2222f2c902a55b
Dvid777 <DSegura2001@gmail.com>
2020-10-30 11:02:59 -0300

    Categoria Evento y Categoria Donaciones services

--- cliente-angular/src/app/services/categoria-evento.service.ts
+++ cliente-angular/src/app/services/categoria-evento.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { ICategoE } from "../models/categoria-evento";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaEventoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getCategoE()
+  {
+    return this.http.get<ICategoE[]>('http://localhost:3000/categoria_eventos');
+  }
+
+  deleteCategoE(id:number)
+  {
+    return this.http.delete('http://localhost:3000/categoria_eventos'+id);
+  }
+
+  saveCategoE(unaCategoE:ICategoE)
+  {
+    return this.http.post('http://localhost:3000/categoria_eventos',unaCategoE);
+  }
+
+  updateCategoE(unaCategoE:ICategoE)
+  {
+    let id:number = unaCategoE.id_categoria_eventos;
+    return this.http.put('http://localhost:3000/categoria_evento'+id,unaCategoE);
+  }
 }
